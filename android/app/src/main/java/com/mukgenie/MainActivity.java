e86383e4c6f20593ba641e1136c7ca1a389802f3
KimMundo01 <801sml@gmail.com>
2024-04-16 14:29:56 +0900

    MainActivity, MainApplication 파일 형식 변경 및 테스트 xml파일

---
+++ android/app/src/main/java/com/mukgenie/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mukgenie;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // 여기에 초기화 및 기타 작업을 추가할 수 있습니다.
+  }
+}
---
+++ android/app/src/main/java/com/mukgenie/MainApplication.java
@@ -0,0 +1,13 @@
+package com.mukgenie;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 여기에 앱의 초기화 코드를 추가할 수 있습니다.
+    }
+}
